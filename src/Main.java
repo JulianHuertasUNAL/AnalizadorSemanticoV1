9f6069da9b81e68c3f9b530169d5ea339979fb75
JulianHuertasUNAL <jhuertasd@unal.edu.co>
2024-06-13 19:49:48 -0500

    Traducción va a un txt: LatinoToPythonTranslation.txt

--- src/Main.java
+++ src/Main.java
@@ -2,27 +2,52 @@
 import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.tree.*;
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        // create a CharStream that reads from standard input / file
-        // create a lexer that feeds off of input CharStream
-        GramaticaLatinoLexer lexer;
+        String outputFilePath = "LatinoToPythonTranslation.txt";
+        try {
+            // Create a FileOutputStream to write to the specified file
+            // If the file exists, it will be overwritten
+            FileOutputStream fos = new FileOutputStream(new File(outputFilePath));
 
-        if (args.length>0)
-            lexer = new GramaticaLatinoLexer(CharStreams.fromFileName(args[0]));
-        else
-            lexer = new GramaticaLatinoLexer(CharStreams.fromStream(System.in));
-        // create a buffer of tokens pulled from the lexer
-        CommonTokenStream tokens = new CommonTokenStream(lexer);
-        // create a parser that feeds off the tokens buffer
-        GramaticaLatinoParser parser = new GramaticaLatinoParser(tokens);
-        ParseTree tree = parser.s(); // begin parsing at init rule
+            // Create a PrintStream that will write to the FileOutputStream
+            PrintStream ps = new PrintStream(fos);
 
-        // Create a generic parse tree walker that can trigger callbacks
-         ParseTreeWalker walker = new ParseTreeWalker();
-         // Walk the tree created during the parse, trigger callbacks
-         walker.walk(new LatinoToPython(), tree);
-         System.out.println(); // print a \n after translation
+            // Redirect System.out to the PrintStream
+            System.setOut(ps);
+
+            // create a CharStream that reads from standard input / file
+            // create a lexer that feeds off of input CharStream
+            GramaticaLatinoLexer lexer;
+
+            if (args.length>0)
+                lexer = new GramaticaLatinoLexer(CharStreams.fromFileName(args[0]));
+            else
+                lexer = new GramaticaLatinoLexer(CharStreams.fromStream(System.in));
+            // create a buffer of tokens pulled from the lexer
+            CommonTokenStream tokens = new CommonTokenStream(lexer);
+            // create a parser that feeds off the tokens buffer
+            GramaticaLatinoParser parser = new GramaticaLatinoParser(tokens);
+            ParseTree tree = parser.s(); // begin parsing at init rule
+
+            // Create a generic parse tree walker that can trigger callbacks
+            ParseTreeWalker walker = new ParseTreeWalker();
+            // Walk the tree created during the parse, trigger callbacks
+            walker.walk(new LatinoToPython(), tree);
+            System.out.println(); // print a \n after translation
+
+            // Flush the stream to ensure all data is written to the file
+            ps.flush();
+
+            // Close the PrintStream and FileOutputStream
+            ps.close();
+            fos.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
