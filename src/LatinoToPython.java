8a83e0f3e3b1f109f703ca1eb4b3cf866afa1944
JulianHuertasUNAL <jhuertasd@unal.edu.co>
2024-06-13 19:37:54 -0500

    Commit inicial del traductor

---
+++ src/LatinoToPython.java
@@ -0,0 +1,7 @@
+public class LatinoToPython extends GramaticaLatinoBaseListener{
+
+    @Override
+    public void enterS(GramaticaLatinoParser.SContext ctx) {
+        System.out.println("Entro a s");
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+// import ANTLR's runtime libraries
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.*;
+import java.io.File;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws Exception {
+        // create a CharStream that reads from standard input / file
+        // create a lexer that feeds off of input CharStream
+        GramaticaLatinoLexer lexer;
+
+        if (args.length>0)
+            lexer = new GramaticaLatinoLexer(CharStreams.fromFileName(args[0]));
+        else
+            lexer = new GramaticaLatinoLexer(CharStreams.fromStream(System.in));
+        // create a buffer of tokens pulled from the lexer
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        // create a parser that feeds off the tokens buffer
+        GramaticaLatinoParser parser = new GramaticaLatinoParser(tokens);
+        ParseTree tree = parser.s(); // begin parsing at init rule
+
+        // Create a generic parse tree walker that can trigger callbacks
+         ParseTreeWalker walker = new ParseTreeWalker();
+         // Walk the tree created during the parse, trigger callbacks
+         walker.walk(new LatinoToPython(), tree);
+         System.out.println(); // print a \n after translation
     }
 }
